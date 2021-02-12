75d7a5471e5b26362c36607cf56d838bcce823af
Uros Kukic <33048701+Kuki145@users.noreply.github.com>
2021-02-12 15:32:50 +0100

    [QSP-10] Lock solidity compiler version (#12)

--- contracts/StarfleetStake.sol
+++ contracts/StarfleetStake.sol
@@ -1,4 +1,4 @@
-pragma solidity >=0.6.0 <=0.8.0;
+pragma solidity 0.6.10;
 
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
