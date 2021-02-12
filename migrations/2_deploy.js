4bcea2a6b59e7975e6d3e0f301be0b2af8e27842
Branimir Rakic <33914812+branarakic@users.noreply.github.com>
2021-02-12 09:39:25 +0100

    Feature/add truffle config (#3)
    * Fix truffle-config.js being ignored in .gitignore

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -5,5 +5,8 @@ module.exports = function (deployer,helper, accounts) {
   deployer.deploy(TestTraceToken).then(function(){
    return deployer.deploy(StarfleetStake,0,TestTraceToken.address);
   });
+
+  // ATRAC deployment
+  //deployer.deploy(StarfleetStake,0,"0x98d9a611ad1b5761bdc1daac42c48e4d54cf5882")
   
 };
